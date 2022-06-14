fix sfm bug and change the scaling for beta KL

module/DeltaETM_module.py
@@ -1027,9 +1027,9 @@ class BayesianETM_module(BaseModuleClass):
     @auto_move_data
     def generative(self, z) -> dict:
 
-        rho, delta, rho_kl, delta_kl  = self.decoder(z)
+        rho, delta, rho_kl, delta_kl, theta  = self.decoder(z)
 
-        return dict(rho = rho, delta = delta, rho_kl = rho_kl, delta_kl = delta_kl)
+        return dict(rho = rho, delta = delta, rho_kl = rho_kl, delta_kl = delta_kl, theta = theta)
     
     # this is for the purpose of computing the integrated gradient, output z but not dict
     def get_latent_representation(
@@ -1046,7 +1046,7 @@ class BayesianETM_module(BaseModuleClass):
             z = inference_out["z"]
         if output_softmax_z:
             generative_outputs = self.generative(z)
-            z = generative_outputs["hh"]      
+            z = generative_outputs["theta"]      
         return z
 
     def get_reconstruction_loss(
@@ -1071,15 +1071,15 @@ class BayesianETM_module(BaseModuleClass):
         """
         inference_out = self.inference(x, y)
         z = inference_out["z"]
-
         gen_out = self.generative(z)
-        
+        theta = gen_out["theta"]
+               
         rho = gen_out["rho"]
-        log_aa_spliced = torch.clamp(torch.mm(z, rho), -10, 10)
+        log_aa_spliced = torch.clamp(torch.mm(theta, rho), -10, 10)
         aa_spliced = torch.exp(log_aa_spliced)
         
         delta = gen_out["delta"]
-        log_aa_unspliced = torch.clamp(torch.mm(z, rho + delta), -10, 10)
+        log_aa_unspliced = torch.clamp(torch.mm(theta, rho + delta), -10, 10)
         aa_unspliced = torch.exp(log_aa_unspliced)
         
         reconstruction_loss_spliced = -self.dir_llik(x, aa_spliced)
@@ -1122,6 +1122,8 @@ class BayesianETM_module(BaseModuleClass):
         the reconstruction loss and the Kullback divergences
 
         """
+        
+        self.sample_size = 217942
         x = tensors[_CONSTANTS.X_KEY]
         y = tensors[_CONSTANTS.PROTEIN_EXP_KEY]
         qz_m = inference_outputs["qz_m"]
@@ -1141,11 +1143,11 @@ class BayesianETM_module(BaseModuleClass):
         kl_divergence_beta = rho_kl + delta_kl
         kl_local = kl_divergence_z
         reconstruction_loss = reconstruction_loss_spliced + reconstruction_loss_unspliced
-        loss = torch.mean(reconstruction_loss + kl_weight * kl_local + kl_weight_beta * kl_divergence_beta/x.size(0)) * x.size(0)
-
+        #loss = torch.mean(reconstruction_loss + kl_weight * kl_local + kl_weight_beta * kl_divergence_beta/x.size(0)) * x.size(0)
+        loss = torch.mean(reconstruction_loss + kl_weight * kl_local) + kl_weight_beta * kl_divergence_beta/self.sample_size
         return LossRecorder(loss, reconstruction_loss, kl_local,
                             reconstruction_loss_spliced=reconstruction_loss_spliced,
                             reconstruction_loss_unspliced=reconstruction_loss_unspliced, 
-                            kl_beta = kl_divergence_beta, 
-                            kl_rho = rho_kl, 
-                            kl_delta = delta_kl,)
+                            kl_beta = kl_divergence_beta/self.sample_size, 
+                            kl_rho = rho_kl/self.sample_size, 
+                            kl_delta = delta_kl/self.sample_size)

nn/base_components.py
@@ -559,6 +559,7 @@ pr = torch.mm(torch.exp(hh),torch.exp(log_beta))
         self,
         z: torch.Tensor,
     ):
+        theta = self.soft_max(z)
         rho = self.get_beta(self.spike_logit_rho, self.slab_mean_rho, self.slab_lnvar_rho, self.bias_k_rho, self.bias_d_rho)
         #log_aa_rho = torch.clamp(torch.mm(z, rho), -10, 10)
         #aa_rho = torch.exp(log_aa_rho)
@@ -569,7 +570,7 @@ pr = torch.mm(torch.exp(hh),torch.exp(log_beta))
         #aa_delta = torch.exp(log_aa_delta)
         delta_kl = self.sparse_kl_loss(self.logit_0_delta, self.lnvar_0_delta, self.spike_logit_delta, self.slab_mean_delta, self.slab_lnvar_delta)
         
-        return rho, delta, rho_kl, delta_kl 
+        return rho, delta, rho_kl, delta_kl, theta 
     
     def get_rho_delta(
         self,